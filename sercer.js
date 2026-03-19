a3d9a6e187e993592cee86d4d80cc3b1f403932e
Tycoontripper1 <oayodeji27@gmail.com>
2026-03-19 17:49:04 +0100

    feat: allow server port to be configured via environment variable with a default fallback.

--- sercer.js
+++ sercer.js
@@ -29,9 +29,10 @@ app.use((err, req, res, next) => {
     })
 })
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5001");
-});
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
 
 
 process.on("SIGINT", () => {
