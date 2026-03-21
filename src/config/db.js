a781a4f86b280a2cb5abb1aec1c4da785d2ca371
Tycoontripper1 <oayodeji27@gmail.com>
2026-03-21 11:23:24 +0100

    feat: Implement Prisma for database management and refactor server entry point.

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
 import 'dotenv/config'
 
-import { PrismaClient } from "@prisma/client";
+
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
