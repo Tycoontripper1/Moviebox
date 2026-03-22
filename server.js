2b39d9e6831a510658ec78afd1074c7912d029ba
Tycoontripper1 <oayodeji27@gmail.com>
2026-03-22 12:04:35 +0100

    feat: configure CORS middleware to specify origin and enable credentials.

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ config.config();
 connectDB();
 
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000", 
+    credentials: true 
+}));
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
