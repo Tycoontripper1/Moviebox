1f8ba490e534eaf638ad2e08ab7eda4cce0818e1
Tycoontripper1 <oayodeji27@gmail.com>
2026-03-23 14:54:09 +0100

    fix: Add HTTPS protocol to Vercel app URL in CORS origin list.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ connectDB();
 
 
 app.use(cors({
-    origin: ["http://localhost:3000","flicklist-eight.vercel.app"], 
+    origin: ["http://localhost:3000", "https://flicklist-eight.vercel.app" ], 
 
     credentials: true 
 }));
